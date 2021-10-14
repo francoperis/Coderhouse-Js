9e63b073c0a4185a52e1f5792ea805e422688e8f
Franco Peris <franco.peris@gmail.com>
2021-10-13 23:51:19 -0300

    me equivoque de index.js en el desafio complementario

--- Clase6/Complementario/index.js
+++ Clase6/Complementario/index.js
@@ -1,42 +1,106 @@
-const ecommerce = [ {id: 1, nombre: "Ryzen 5600x", precio: 40000, stock: true},
-                    {id: 2, nombre: "Mother ASUS 450", precio: 10000, stock: false},
-                    {id: 3, nombre: "RAM 8GB HYPERX", precio: 7000, stock: true},
-                    {id: 4, nombre: "SSD 240GB", precio: 5500, stock:true}
-                ];
+// desafio arrays, mostrando objetos, clases, y arrays   este desafio contiene la parte del complementario 
 
-class Producto {
-    constructor (producto) {
-        this.id = producto.id;
-        this.nombre = producto.nombre;
-        this.precio = producto.precio;
-        this.stock = producto.stock;
-    }
+alert("Bienvenido a Panera Rosa");
+
+
+
+class Producto{ // creamos la clase producto y  agregamos los objetos. 
+    constructor (id, nombreProducto, PrecioProducto, stock) {// cada clase va a tener estas propiedades , 
+
+  this.idProducto = id;
+  this.nombre = nombreProducto;
+  this.PrecioProducto = PrecioProducto;
+  this.stock = stock; 
+} 
+} 
+
+
+// instanciamos los objetos, y los pasamos como parametros. 
+let Producto1 = new Producto(001, "Torta Cheesecake oreo", 4000, 8);
+let Producto2 = new Producto(002, "Torta marquisse de chocolate", 5500, 5);
+let Producto3 = new Producto(003, "torta lemon pie ", 4500, 7);
+let Producto4 = new Producto(004, "torta Carrot-Cake ", 4500, 3);
+let Producto5 = new Producto(005, "torta Choco-torta ", 4500, 10);
+let Producto6 = new Producto(006, "torta cheesecake frutos rojos", 4500, 9);
+
+let ArrayProductos = [Producto1,Producto2,Producto3,Producto4,Producto5,Producto6]; // creamos/ inicializamos  el arreglo. 
+
+console.log(ArrayProductos);
+
+
+let Carrito = []; // creamos otro arreglo para el carrito. 
+
+function Principal() {
+
+    let opcionUsuario = mostrarProductos();
+
+    while (opcionUsuario != 0){
+
+        switch(opcionUsuario){
+        case "1": 
+        alert("Agregaste el producto: " + ArrayProductos[0].nombreProducto)
+        agregarProducto(ArrayProductos[0]); 
+        break// le mandamos el objeto producto que esta dentro de la posicion n del arreglo. 
+    
+   
+        case "2": 
+        alert("Agregaste el producto: " + ArrayProductos[1].nombreProducto)
+        agregarProducto(ArrayProductos[1]);
+        break
+
+        case "3": 
+        alert("Agregaste el producto: " + ArrayProductos[2].nombreProducto)
+        agregarProducto(ArrayProductos[2]);
+        break
+
+        case "4": 
+        alert("Agregaste el producto: " + ArrayProductos[3].nombreProducto)
+        agregarProducto(ArrayProductos[3]);
+        break
+
+        case "5": 
+        alert("Agregaste el producto: " + ArrayProductos[4].nombreProducto)
+        agregarProducto(ArrayProductos[4]);
+        break
+
+        case "6": 
+        alert("Agregaste el producto: " + ArrayProductos[5].nombreProducto)
+        agregarProducto(ArrayProductos[5]);
+        break
+
+        default:
+            alert("DATO INCORRECTO, POR FAVOR INGRESE UNA DE LAS OPCIONES!")
+     
+        } 
+
+        opcionUsuario = mostrarProductos();
+
+
+    } 
+
+    console.log(Carrito);
+
+
+
+} 
+
+
+
+function mostrarProductos() {
+
+    let opcionUsuario = parseInt(prompt("SELECCIONE QUE TIPO DE TORTA desea agregar: o  presiona 0 para salir: \n\n (PRESIONE 1) cheesecake-oreo \n\n (PRESIONE 2)- cheesecake frutos rojos \n\n (PRESIONE 3)-lemon pie \n\n (PRESIONE 4)- marquisse de chocolate \n\n (PRESIONE 5)- chocotorta \n\n(PRESIONE6)- carrot-cake" ));
+    return opcionUsuario;
+
+
+
 }
 
-const carrito = []
 
-const comprar = (nombreProducto) =>{
-    let producto = ecommerce.find(producto => producto.nombre == nombreProducto)
-    if(producto){
-        carrito.push(producto);
-    }
-    else{
-        console.log("lo siento, no tenemos stock de este producto.")
-    }
-    }
+function agregarProducto(Producto){
+    Carrito.push(Producto);
 
-const Fuente600w = new Producto ({id:5, nombre:"Fuente600w", precio:6600, stock:true});
-ecommerce.push(Fuente600w)
+}
 
-comprar("SSD 240GB")
-comprar("RTX 3090")
-comprar("Mother ASUS 450")
 
-console.log(carrito)
 
-const FiltroNombre = (nombreProducto)=>{
-    const buscar = ecommerce.filter(producto => producto.nombre === nombreProducto)
-    console.log(buscar)
-};
-
-FiltroNombre ("Fuente600w");
+Principal();
