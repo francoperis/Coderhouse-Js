947054a896b7a9a7a72492a4de7b494187cf7b48
Franco Peris <franco.peris@gmail.com>
2021-10-13 23:04:12 -0300

    Prototipo Carrito

---
+++ funcionclasesafter/index.js
@@ -0,0 +1,18 @@
+class jugadorFutbol{
+    constructor(nombre, edad, altura, peso){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.altura=altura;
+        this.peso=peso;
+    }
+
+    getEdad(){
+
+    }
+    getAltura(){
+
+    }
+    getPeso(){
+        
+    }
+}
