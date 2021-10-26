89fee83b85ad641f00e9ff8fc62a79fd07062557
Franco Peris <franco.peris@gmail.com>
2021-10-25 22:23:51 -0300

    Desafio 8

---
+++ Clase8/desafio8.js
@@ -0,0 +1,12 @@
+let h1 =document.getElementById("titulo");
+console.log(h1.innerHTML);
+titulo.style.background ="red";
+
+
+let div =document.getElementById("texto");
+texto.style.font ="italic bold 30px arial"
+
+
+//crear un elemento
+var tit =document.createElement("h3");
+tit.innerText = "esto es un titulo mas pequeño"
